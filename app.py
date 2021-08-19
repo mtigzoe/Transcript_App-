57a5c63ee4d38622c8109fdb5e33542ed6f63440
David Phelps <dphelps3@my.wctc.edu>
2021-08-19 05:26:03 -0500

    Filtered text values from transcript

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,request
 from youtube_transcript_api import YouTubeTranscriptApi
-from youtube_transcript_api.formatters import JSONFormatter
+from youtube_transcript_api.formatters import WebVTTFormatter
 import re
 import pafy   
 
@@ -22,26 +22,18 @@ def result():
         source_video_id = pafy.new(result)
         video_id = source_video_id.videoid
         title = source_video_id.title
+        author = source_video_id.author
+        upload_date = source_video_id.published
         
         transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
+        script = ""
 
-        formatter = JSONFormatter()
+        for text in transcript:
+            t = text["text"]
+            if t != '[Music]':
+                script += t + " "
 
-        data = formatter.format_transcript(transcript, indent=2)
-
-        #lines = []
-        #for item in data[""]:
-        #    lines.append([item["text"]])
-        #    return render_template("result.html", title=title, len=len(lines), lines=lines)
-
-        return render_template("result.html", title=title, data=data)
-
-        #srt = YouTubeTranscriptApi.get_transcript(video_id)
-        #lines = []
-        #for i in srt:
-        #    lines.append(i)
-        #return render_template("result.html", len = len(lines), lines= lines)
-
+        return render_template("result.html", title=title, author=author, upload_date=upload_date, script=script)
 
 if __name__ == '__main__':
     app.run(debug=True)    
