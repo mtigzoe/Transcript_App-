4cf3bc3ba970702f42c9f1dc2b99b6b3d2c63544
David Phelps <dphelps3@my.wctc.edu>
2021-08-18 21:08:26 -0500

    first stab

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request
 from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import JSONFormatter
 import re
 import pafy   
 
@@ -12,19 +13,34 @@ def form():
     return render_template('form.html')
 
 
-@app.route('/result',methods = ['POST', 'GET'])
+@app.route('/result', methods = ['POST', 'GET'])
 def result():
     if request.method == 'POST':
-        #result = request.form
+
         result = request.form.get("yt_url")
       
         source_video_id = pafy.new(result)
         video_id = source_video_id.videoid
-        srt = YouTubeTranscriptApi.get_transcript(video_id)
-        lines = []
-        for i in srt:
-            lines.append(i)
-        return render_template("result.html", len = len(lines), lines= lines)
+        title = source_video_id.title
+        
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
+
+        formatter = JSONFormatter()
+
+        data = formatter.format_transcript(transcript, indent=2)
+
+        #lines = []
+        #for item in data[""]:
+        #    lines.append([item["text"]])
+        #    return render_template("result.html", title=title, len=len(lines), lines=lines)
+
+        return render_template("result.html", title=title, data=data)
+
+        #srt = YouTubeTranscriptApi.get_transcript(video_id)
+        #lines = []
+        #for i in srt:
+        #    lines.append(i)
+        #return render_template("result.html", len = len(lines), lines= lines)
 
 
 if __name__ == '__main__':
