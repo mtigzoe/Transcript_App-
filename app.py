6cb4755ce406a434099e0b73fddcafcf6d26f246
mtigzoe <mloznea@g.clemson.edu>
2021-09-16 11:16:59 -0400

    app.py, index.html and result.html

--- app.py
+++ app.py
@@ -1,50 +1,40 @@
 from flask import Flask,render_template,request
 from youtube_transcript_api import YouTubeTranscriptApi
-from youtube_transcript_api.formatters import JSONFormatter
 import re
 import pafy   
 
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def form():
     return render_template('form.html')
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 
-@app.route('/result', methods = ['POST', 'GET'])
+@app.route('/result',methods = ['POST', 'GET'])
 def result():
     if request.method == 'POST':
-
         result = request.form.get("yt_url")
-      
         source_video_id = pafy.new(result)
         video_id = source_video_id.videoid
         title = source_video_id.title
+        author = source_video_id.author
+        #upload_date = source_video_id.published
+        script = ""
         
-        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
+        try:
+            YouTubeTranscriptApi.get_transcript(video_id)
+            transcript = YouTubeTranscriptApi.get_transcript(video_id)
+            for text in transcript:
+                t = text["text"]
+                if t != '[Music]':
+                    script += t + " "
+            return render_template("result.html", script=script)
 
-        formatter = JSONFormatter()
-
-        data = formatter.format_transcript(transcript, indent=2)
-
-        #lines = []
-        #for item in data[""]:
-        #    lines.append([item["text"]])
-        #    return render_template("result.html", title=title, len=len(lines), lines=lines)
-
-        return render_template("result.html", title=title, data=data)
-
-        #srt = YouTubeTranscriptApi.get_transcript(video_id)
-        #lines = []
-        #for i in srt:
-        #    lines.append(i)
-        #return render_template("result.html", len = len(lines), lines= lines)
-
+        except:
+            return render_template('index.html')
 
 if __name__ == '__main__':
-   
-    app.run(debug=True)
-  
-  
+    app.run(debug=True)    
